1ff6fe5cbca577814ae989e5535f8ac5919b9b90
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-03-27 19:08:39 +0900

    2장 정리 끝

--- 1_Semester/Javascript/main.js
+++ 1_Semester/Javascript/main.js
@@ -1,7 +1,4 @@
-const arr2 = ['가','라','다','라','마','라']
+const betty = x => !!x;
 
-for (let index = 0; index < arr2.length; index++) {
-  const ra1 = arr2.indexOf('라');
-  arr2.splice(ra1,1);
-}
-console.log(arr2);
+
+console.log(betty (3));
