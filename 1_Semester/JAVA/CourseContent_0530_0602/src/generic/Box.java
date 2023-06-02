7b4c3801f9ee573bdf7ea10713f7f269ee6fcef8
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-06-02 17:48:17 +0900

    Ramda + Generic

---
+++ 1_Semester/JAVA/CourseContent_0530_0602/src/generic/Box.java
@@ -0,0 +1,17 @@
+package generic;
+
+public class Box {
+  
+  Object value;  // 모든 형을 받을 수 있는 Object형
+
+  public Object getValue() {  // 모든 형을 받을 수 있는 Object형
+    return value;
+  }
+
+  public void setValue(Object value) { // 모든 형을 받을 수 있는 Object형
+    this.value = value;
+  }
+  
+}
+
+
