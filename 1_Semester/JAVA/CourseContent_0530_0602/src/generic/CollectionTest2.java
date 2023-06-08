98be530fcb19d238117cfedd7e657c4ef4c32fa0
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-06-08 12:53:47 +0900

    UPDATE

---
+++ 1_Semester/JAVA/CourseContent_0530_0602/src/generic/CollectionTest2.java
@@ -0,0 +1,58 @@
+package generic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CollectionTest2 {
+   public static void main(String[] args) {
+    // HashMap =?  Java 에서 javascript 객체 처럼 key : value의 쌍으로 사용할 수 있는 것
+      HashMap<String,Integer> nameAge = new HashMap<>();
+      nameAge.put("이재일",25);  // 키와 값의 쌍으로 데이터를 저장
+      int age = nameAge.get("이재일"); // 키의 값으로 그에 대응되는 값을 인출
+      
+      // int age = nameAge.get["이재일"] // 이렇게 연관 배열로는 호출 불가하다.......
+      System.out.println("이재일의 나이는> => "+ age );
+      
+      
+      nameAge.put("이상열",58);
+      age = nameAge.get("이상열");
+      System.out.println("이상열의 나이는> => "+ age );
+      
+      nameAge.put("박은영",56);
+      age = nameAge.get("박은영");
+      System.out.println("박은영의 나이는> => "+ age );
+
+      System.out.println("------------------------------------------------------------");
+
+      HashMap<String,ArrayList<String>> teams = new HashMap<>();
+      ArrayList<String> team1 = new ArrayList<>();
+      team1.add("이종열");
+      team1.add("이현숙");
+      team1.add("이규석");
+      team1.add("이아현");
+      team1.add("이지민");
+
+      ArrayList<String> team2 = new ArrayList<>();
+      team2.add("이동열");
+      team2.add("박지헌");
+      team2.add("이진혁");
+      team2.add("이예랑");
+
+
+      teams.put("이종열네",team1);
+      teams.put("이동열네",team2);
+
+      printTeamMember("이동열네",teams);
+   }
+
+  public static void printTeamMember(String teamName, HashMap<String,ArrayList<String>> teams) {
+      ArrayList<String> members = teams.get(teamName);
+      System.out.println(teamName+"의 멤버들");
+
+      for (String member:members) {
+        System.out.println(member);
+      }
+      System.out.println();
+  }
+
+}
---
+++ 1_Semester/JAVA/CourseContent_0530_0602/src/generic/GenericTest2.java
@@ -0,0 +1,24 @@
+package generic;
+
+public class GenericTest2 {
+  public static void main(String[] args) {
+    // GenericPair 클래스 객체 2개 생성해서 값을 넣고 빼고 해라
+
+    GenericPair<String,Integer> gp = new GenericPair<>("ICHIBAN",30);
+    String key = gp.getKey();
+    int val = gp.getValue(); // outo unboxing
+    
+    /* Generic 클래스의 타입 파라미터 값으로는 반드시 참조형 타입이 와야 한다.
+
+    * 기초자료형 (Primitive type)은 절대 올 수 없다. 🔻*/
+    // GenericPair<String,int> gp2 = new GenericPair<>("ICHIBAN",30);
+
+
+    // 타입 파라미터로 기초라요형의 배열은 올 수 있나❓❓❓  -> ⭕⭕⭕ 올수 있다  🔻🔻🔻🔻🔻
+    int[] jumsus = {90,87,88,79,94};
+    GenericPair<String,int[]> pg3= new GenericPair<String,int[]>("점수임", jumsus); 
+    
+
+
+  }
+}
