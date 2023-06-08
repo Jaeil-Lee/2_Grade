24188b6d3670a312fc97dfb4201fb8afcd686ee0
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-06-08 15:21:34 +0900

    UPDATE

--- 1_Semester/Javascript/test.js
+++ 1_Semester/Javascript/test.js
@@ -1,6 +1,19 @@
-const candidate = Array(45).fill().map((v, i) => i + 1);
-console.log(candidate)
-console.log(candidate.splice(3,1))
+// const candidate = Array(45).fill().map((v, i) => i + 1);
+// console.log(candidate)
+// console.log(candidate.splice(3,1))
 
-
-
+const { body } = document;
+const $table = document.createElement('table');
+const $result = document.createElement('div'); // 결과창
+const rows = [];
+for (let i = 1; i <= 5; i++) {
+ const $tr = document.createElement('tr');
+ const cells = [];
+ for (let j = 1; j <= 4; j++) {
+ const $td = document.createElement('td');
+ cells.push($td);
+ $tr.appendChild($td);
+ }
+ rows.push(cells);
+ $table.appendChild($tr);
+}
