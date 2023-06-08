dcb2192ff9ac98c693bf2641df166e66ee1b46e1
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-06-08 11:12:50 +0900

    UPDATE

--- 1_Semester/JAVA/CourseContent_0530_0602/src/generic/Box.java
+++ 1_Semester/JAVA/CourseContent_0530_0602/src/generic/Box.java
@@ -1,8 +1,8 @@
 package generic;
 
 public class Box {
-  
-  Object value;  // 모든 형을 받을 수 있는 Object형
+   
+  Object value;  // 모든 형을 받을 수 있는 Object형  -> 모든 객체를 다 넣을 수 있다- > 이래 작성하면 효율적이다 ...
 
   public Object getValue() {  // 모든 형을 받을 수 있는 Object형
     return value;
